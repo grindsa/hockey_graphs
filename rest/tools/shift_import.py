365e773bcaabfe85c53f11195eaa322a9b76c2ad
grindsa <grindelsack@gmail.com>
2020-10-04 18:33:25 +0200

    [fix] debug mode in shift_import.py

--- rest/tools/shift_import.py
+++ rest/tools/shift_import.py
@@ -6,10 +6,16 @@ import sys
 import os
 import json
 sys.path.insert(0, '..')
+from rest.functions.helper import logger_setup
 from rest.functions.shift import shift_add
 from rest.functions.periodevent import periodevent_add
 
 if __name__ == '__main__':
+
+    DEBUG = True
+
+    # initialize logger
+    LOGGER = logger_setup(DEBUG)
 
     DATA_PATH = '../data/2019/matches'
 
@@ -22,7 +28,7 @@ if __name__ == '__main__':
                 #print('process', shift_file)
                 with open(SHIFT_FILE, encoding='utf8') as shift_file_obj:
                     shift_dic = json.load(shift_file_obj)
-                    shift_id = shift_add('match_id', int(ele), {'match_id': int(ele), 'shift': shift_dic})
+                    shift_id = shift_add(LOGGER, 'match_id', int(ele), {'match_id': int(ele), 'shift': shift_dic})
             else:
                 print('NOT FOUND!', SHIFT_FILE)
                 shift_dic = {}
@@ -31,6 +37,6 @@ if __name__ == '__main__':
             if os.path.isfile(EVENT_FILE):
                 with open(EVENT_FILE, encoding='utf8') as event_file_obj:
                     event_dic = json.load(event_file_obj)
-                    event_id = periodevent_add('match_id', int(ele), {'match_id': int(ele), 'period_event': event_dic})
+                    event_id = periodevent_add(LOGGER, 'match_id', int(ele), {'match_id': int(ele), 'period_event': event_dic})
             else:
                 print('NOT FOUND!', EVENT_FILE)
