4221090c6465b602347e5f0398e7ba937b68540d
grindsa <grindelsack@gmail.com>
2020-10-19 07:39:06 +0200

    [ui] codecomments

--- ui/src/app/app.jsx
+++ ui/src/app/app.jsx
@@ -7,12 +7,10 @@ import { GET, POST, asyncGET } from '../components/fetch.js';
 const rest_url = 'http://127.0.0.1:8081/api/v1/';
 
 export class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
       endpoints: [],
-      matchdays: {},
     }
   }
 
--- ui/src/components/matchday.jsx
+++ ui/src/components/matchday.jsx
@@ -3,7 +3,7 @@ import { changeMatchDay }  from './matchdaystateservice';
 import { asyncGET } from './fetch.js';
 
 export class MatchDayList extends React.Component {
-
+  /* class which creates and displays list of matches for cert day */
   constructor(props) {
     super(props);
     this.state = {
@@ -17,6 +17,7 @@ export class MatchDayList extends React.Component {
   }
 
   async componentDidUpdate(prevProps) {
+    /* we get the url to fectch as props and monitor it here */
     if (this.props.url !== prevProps.url) {
         // get matchdays
         const matchdaylist = await asyncGET(this.props.url)
@@ -24,23 +25,15 @@ export class MatchDayList extends React.Component {
     }
   }
 
-  async getData(apiEndpoint, parameter) {
-    if(apiEndpoint){
-      const { data: Items } = await GET(apiEndpoint);
-      if (Items) {
-        this.setState({[parameter]: Items });
-      }else{
-        // error
-      }
-    }
-  }
-
   filterMatchDay(Dictionary){
-    // console.log(Dictionary)
+    /* filter certain matchday out of matchdaylist */
     var MatchDay = []
     for(var keyName in Dictionary){
+        /* skip all matchdays with displayday false */
         if(Dictionary[keyName]['displayday']){
+          /* list of days will be stored in a local variable as it is not needed in states */
           MatchDay = Dictionary[keyName]['matches']
+          /* we need these variables in other functions thus lets put them into state */
           this.state.date = Dictionary[keyName]['date']
           this.state.currentKeyName = keyName
           this.state.previousKeyName = Dictionary[keyName]['previous']
@@ -51,6 +44,7 @@ export class MatchDayList extends React.Component {
   }
 
   handleMatchDayChange(newDay){
+    /* this is a handler for a matchday change we call a function and update the state */
     this.setState(currentState => {
       return {
         ... currentState,
@@ -65,7 +59,6 @@ export class MatchDayList extends React.Component {
         <p key={Match.match_id}>{Match.home_team} <img src={Match.home_team_logo} alt={Match.home_team} />{Match.result} <img src={Match.visitor_team_logo} alt={Match.visitor_team} />{Match.visitor_team}</p>
       )
     });
-
     return (
       <React.Fragment>
         <ChangeMatchday
@@ -81,6 +74,7 @@ export class MatchDayList extends React.Component {
 }
 
 export class ChangeMatchday extends React.Component {
+  /* this class displays a header allowing matchday changes */
   render(){
     return(
       <p>
