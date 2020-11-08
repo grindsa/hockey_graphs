d377d9509266ee0e39f2351a31a3d8e71f37cccd
grindsa <grindelsack@gmail.com>
2020-11-08 07:08:53 +0100

    [rest] lineup_sort()

--- rest/functions/lineup.py
+++ rest/functions/lineup.py
@@ -1,10 +1,56 @@
 # -*- coding: utf-8 -*-
 
+def _rosterid_rebuild(logger, roster_id):
+    """ created sorted lineup from roster_list """
+    # logger.debug('_rosterid_rebuild({0})'.format(player_id))
+
+    # split id into different pieces
+    playerposition = int(roster_id[0])
+    line = int(roster_id[1])
+    lineposition = int(roster_id[2])
+
+    if playerposition != 1:
+        if playerposition == 3:
+            # forwards first
+            playerposition = 0
+        # new player_id
+        new_id = int('{0}{1}{2}'.format(line, playerposition, lineposition))
+    else:
+        new_id = None
+
+    return new_id
+
 def lineup_sort(logger, roster_list):
     """ created sorted lineup from roster_list """
     logger.debug('lineup_sort()')
 
-    lineup_dic = {'home_team': {}, 'guest_team': {}}
+    lineup_dic = {'home_team': {}, 'visitor_team': {}}
+    player_dic = {}
 
-    # from pprint import pprint
-    # pprint(roster_list)
+    for team in roster_list:
+
+        # decide and harmonize on team_name (location in hash)
+        if team == 'home':
+            team_name = 'home_team'
+        else:
+            team_name = 'visitor_team'
+
+        _tmp_dic = {}
+        for roster_id in roster_list[team]:
+            # we need to rebuild player_id and rearrange for easier sorting
+            _id = _rosterid_rebuild(logger, roster_id)
+            if _id:
+                # skip goalies
+                _tmp_dic[_id] = roster_id
+
+
+        # store entries sorted by key( new player_id) and initialize counter for faster access
+        cnt = 0
+        for key in sorted(_tmp_dic):
+            cnt += 1
+            lineup_dic[team_name][cnt] = roster_list[team][_tmp_dic[key]]
+            # hope thats correct we need a dictionary to lookup the position in lineup-dic based on player_id
+            player_dic[roster_list[team][_tmp_dic[key]]['playerId']] = cnt
+            # print(roster_list[team][_tmp_dic[key]]['playerId'], cnt)
+
+    return (lineup_dic, player_dic)
