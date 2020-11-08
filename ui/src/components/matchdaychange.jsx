d167ceffaf6a2a6fb86e44f25f1eb6775d5d5412
grindsa <grindelsack@gmail.com>
2020-11-08 04:55:02 +0100

    [ui] ChangeMatchday - table alignment

--- ui/src/components/matchdaychange.jsx
+++ ui/src/components/matchdaychange.jsx
@@ -19,23 +19,28 @@ export class ChangeMatchday extends React.Component {
 
   render(){
     return(
-      <div className="w3-container w3-padding scolor w3-center">
-        <h1>
-          <i className="fa fa-calendar fa-lg w3-margin-left w3-left" onClick={() => this.toggleCalendar()}/>
-          <Calendar
-            display={this.state.showCalendar}
-            toggleCalendar={this.toggleCalendar}
-            language={this.props.language}
-            matchdaylist={this.props.matchdaylist}
-            current={this.props.current}
-            date={this.props.date}
-            handleDayClick = {this.props.handleDayClick}
-          />
-          <a href='#' onClick={() => this.props.onChangeMatchDay(this.props.previous)}><i className="w3-margin-right fa fa-arrow-left" /></a>
-           {this.props.date}
-          <a href='#' onClick={() => this.props.onChangeMatchDay(this.props.next)}><i className="w3-margin-left fa fa-arrow-right" /></a>
-        </h1>
-      </div>
+      <div className="w3-container w3-padding scolor">
+        <div className="w3-col w3-left w3-padding-top" style={{width:'10%'}}>
+            <i className="w3-padding-top fa fa-calendar w3-large fa-lg w3-margin-left w3-left" onClick={() => this.toggleCalendar()}/>
+            <Calendar
+              display={this.state.showCalendar}
+              toggleCalendar={this.toggleCalendar}
+              language={this.props.language}
+              matchdaylist={this.props.matchdaylist}
+              current={this.props.current}
+              date={this.props.date}
+              handleDayClick = {this.props.handleDayClick}
+            />
+        </div>
+        <div className="w3-col w3-center" style={{width:'80%'}}>
+          <h1>
+            <a href='#' onClick={() => this.props.onChangeMatchDay(this.props.previous)}><i className="w3-margin-right fa fa-arrow-left" /></a>
+             {this.props.date}
+            <a href='#' onClick={() => this.props.onChangeMatchDay(this.props.next)}><i className="w3-margin-left fa fa-arrow-right" /></a>
+          </h1>
+        </div>
+        <div className="w3-col w3-left" style={{width:'10%'}}></div>
+    </div>
     )
   }
 }
