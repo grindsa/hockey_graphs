dd8b45035313c610404a6350bb6cfe8194fb0884
grindsa <grindelsack@gmail.com>
2020-11-13 06:49:48 +0100

    [fix] seasonname length

--- rest/models.py
+++ rest/models.py
@@ -5,7 +5,7 @@ import jsonfield
 # Create your models here.
 class Season(models.Model):
     """ season table """
-    name = models.CharField(max_length=15)
+    name = models.CharField(max_length=20)
     def __str__(self):
         return self.name
 
