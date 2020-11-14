421dd948d1b56e11c001d62fdde958640e3a53f7
grindsa <grindelsack@gmail.com>
2020-11-14 20:18:22 +0100

    [ui] season selector

--- ui/src/app/app.jsx
+++ ui/src/app/app.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 import Cookies from 'universal-cookie';
-import { ChangeMatchday, MatchDayList } from '../components/matchday'
-import { LanguageSelector } from '../components/languageselector'
+import { ChangeMatchday, MatchDayList } from '../components/matchday';
+import { LanguageSelector } from '../components/languageselector';
+import { SeasonSelector } from '../components/seasonselector';
 import { GET, POST } from '../components/fetch.js';
 import { asyncGET } from '../components/sharedfunctions.js';
 import '../css/mytheme.css';
@@ -18,24 +19,52 @@ export class App extends React.Component {
     super(props);
     this.state = {
       endpoints: [],
+      seasonlist: [],
       language: 'DE',
+      selectedSeason: 0,
     }
+
+    this.changeSeason = this.changeSeason.bind(this);
   }
 
   async componentDidMount(){
     // get rest endpoints
     const endpoints = await asyncGET(rest_url, 'endpoints')
     this.setState({endpoints: endpoints });
+    // get and set seasonlist
+    const seasonlist = await asyncGET(endpoints.seasons)
+    await this.setState({seasonlist: seasonlist});
+    // seasonid
+    if (this.state.selectedSeason === 0){
+      var seasonid = this.state.seasonlist.results[this.state.seasonlist.count-1].id
+      await this.setState({selectedSeason: seasonid});
+    }
+
+    // parse cookie
     const cookies = new Cookies();
     const langValue = cookies.get(app_name).language
+    var selectedSeason = cookies.get(app_name).selectedSeason
+    if(selectedSeason){
+      await this.setState({selectedSeason: selectedSeason});
+    }
     await this.setState({language: langValue });
+  }
+
+  async changeSeason(newSeason){
+    // change season
+    await this.setState({selectedSeason: newSeason})
+    // update cookie
+    const cookies = new Cookies();
+    cookies.set(app_name, {language: this.state.language, selectedSeason: this.state.selectedSeason, foo: 'WannaSeeUrFaceOnceUreadThis'}, { path: '/', maxAge: 2419200 });
   }
 
   async toggleLanguage(){
     let { language } = this.state;
+    // change language
     await this.setState({ language: language === 'DE' ? 'EN' : 'DE' });
+    // update cookie
     const cookies = new Cookies();
-    cookies.set(app_name, {language: this.state.language, foo: 'WannaSeeUrFaceOnceUreadThis'}, { path: '/', maxAge: 2419200 });
+    cookies.set(app_name, {language: this.state.language, selectedSeason: this.state.selectedSeason, foo: 'WannaSeeUrFaceOnceUreadThis'}, { path: '/', maxAge: 2419200 });
   }
 
   render() {
@@ -43,7 +72,8 @@ export class App extends React.Component {
       <div className="mainwidth">
         <div className="w3-bar w3-padding pcolor">
           <i className="fa fa-bars fa-lg w3-xlarge  w3-bar-item" />
-          <LanguageSelector langValue={ this.state.language } onClick={() => this.toggleLanguage()} />     
+          <SeasonSelector seasonValue={this.state.selectedSeason} seasonlist={ this.state.seasonlist.results } onchangeSeason={ this.changeSeason } />
+          <LanguageSelector langValue={ this.state.language } onClick={() => this.toggleLanguage()} />
         </div>
         <MatchDayList matchdays={this.state.endpoints.matchdays} matchstatistics={this.state.endpoints.matchstatistics} language={this.state.language}/>
       </div>
---
+++ ui/src/components/seasonselector.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+export class SeasonSelector extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  getShortcut(seasonValue, seasonlist){
+    var seasonname = ''
+    var color = 'pcolor'
+    if (seasonValue != 0){
+      for(var key in seasonlist) {
+        if (seasonlist[key].id === seasonValue){
+          seasonname = seasonlist[key].name
+          // color pink if magenta sponsored events get selected
+          if (seasonlist[key].name.includes('agenta')){
+            color = 'w3-pink'
+          }
+          break
+        }
+      }
+    }
+    const classNames = 'w3-tag w3-round w3-border ' + color
+    return[seasonname, classNames]
+  }
+
+  render() {
+    const [seasonname, classNames] = this.getShortcut(this.props.seasonValue, this.props.seasonlist)
+    return (
+      <div className = "w3-dropdown-hover w3-left">
+        <button className = "w3-button">
+          <span className={classNames}>{seasonname}</span>
+        </button>
+        <SeasonList seasonlist={ this.props.seasonlist } onchangeSeason={ this.props.onchangeSeason }/>
+      </div>
+    );
+  }
+}
+
+class SeasonList extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    if (this.props.seasonlist){
+      var mlist = this.props.seasonlist.map((season, index) =>{
+        return(<div key={index} className = "w3-bar-item w3-button" onClick={() => this.props.onchangeSeason(season.id)}>{season.name}</div>)
+      });
+    }else{
+      let mlist
+    }
+    return(
+      <div className = "w3-dropdown-content w3-bar-block w3-border pcolor">
+        {mlist}
+      </div>
+    )
+  }
+}
