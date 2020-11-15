e9c77c64f790e70166fa71c3ee943def20c742be
grindsa <grindelsack@gmail.com>
2020-11-15 18:13:57 +0100

    [rest] enable OTP in admin view

--- rest/urls.py
+++ rest/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     path('', include(router.urls)),
     # url(r'^matchdays/(?P<resource_id>\d+)[/]?$', views.MatchDayViewSet.as_view(), name='MatchDayViewSet'),
     # url(r'^matchdays[/]?$', views.MatchDayViewSet.as_view(), name='MatchDayViewSet'),
-    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    #url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
