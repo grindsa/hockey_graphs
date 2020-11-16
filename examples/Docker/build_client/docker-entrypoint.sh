8938af2c977c5ad20728fa5a16be92a3c338f4b9
grindsa <grindelsack@gmail.com>
2020-11-16 14:05:44 +0100

    [base] deploy option in docker_entrypoint.sh

--- examples/Docker/build_client/docker-entrypoint.sh
+++ examples/Docker/build_client/docker-entrypoint.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 # entrypoint script to build a new bundle and push it to production
+P1=$1
+
 cd /tmp/ui
 npm install
 npm install --only=dev
 npm run compile
 
-# sync data to prod
-cd /tmp
-rsync -avze 'ssh -i id_ed25519 -o "StrictHostKeyChecking=no"' ./rest hockeygraphs@hockeygraphs.dynamop.de:~
-rsync -avze 'ssh -i id_ed25519 -o "StrictHostKeyChecking=no"' ./ui/dist hockeygraphs@hockeygraphs.dynamop.de:ui
 
-# apply schema migrations
-ssh -i id_ed25519 hockeygraphs@hockeygraphs.dynamop.de 'python3 manage.py makemigrations && python3 manage.py migrate'
+if [ $1 = "deploy" ]; then
+    echo "deploy"
+    # sync data to prod
+    cd /tmp
+    rsync -avze 'ssh -i id_ed25519 -o "StrictHostKeyChecking=no"' ./rest hockeygraphs@hockeygraphs.dynamop.de:~
+    rsync -avze 'ssh -i id_ed25519 -o "StrictHostKeyChecking=no"' ./ui/dist hockeygraphs@hockeygraphs.dynamop.de:ui
 
-# restart prod
-ssh -i id_ed25519 hockeygraphs@hockeygraphs.dynamop.de sudo '/etc/init.d/apache2 restart'
+    # apply schema migrations
+    ssh -i id_ed25519 hockeygraphs@hockeygraphs.dynamop.de 'python3 manage.py makemigrations && python3 manage.py migrate'
 
-exec "$@"
+    # restart prod
+    ssh -i id_ed25519 hockeygraphs@hockeygraphs.dynamop.de sudo '/etc/init.d/apache2 restart'
+
+else
+    echo "no deploy"
+fi
+
+# exec "$@"
--- examples/Docker/build_client/hockeygraph_deploy.sh
+++ examples/Docker/build_client/hockeygraph_deploy.sh
@@ -2,9 +2,13 @@
 # deploy script for hockeygraphs.dynamop.deploy
 
 BUILD_DIR='/tmp/build'
+ID_FILE="$HOME/.ssh/id_ed25519"
+
 HOCKEYGRAPHS_MASTER='https://github.com/grindsa/hockey_graphs/archive/master.tar.gz'
 ARCHIVE_NAME='hockey_graphs-master'
-ID_FILE="$HOME/.ssh/id_ed25519"
+CONTAINER_NAME='build_client'
+
+
 # create temporary directory
 mkdir -p $BUILD_DIR
 
@@ -16,11 +20,16 @@ tar xvfz $BUILD_DIR/$ARCHIVE_NAME.tgz -C $BUILD_DIR
 cd $BUILD_DIR/$ARCHIVE_NAME
 
 # build image
-# docker build -f examples/Docker/build_client/Dockerfile -t build_client . --no-cache 
+docker build -f examples/Docker/build_client/Dockerfile -t build_client . --no-cache 
 
 # start container
-docker run -v $ID_FILE:/tmp/id_ed25519 --name=build_client build_client
+docker run -v $ID_FILE:/tmp/id_ed25519 --name=build_client build_client deploy
 
-
+# cleanup
+# stop and remove container
+docker container stop build_client
+docker container rm build_client
+# remove image
+docker image rm build_client
 
 exit 0
