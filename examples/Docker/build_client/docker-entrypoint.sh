c369d3289aa4fe1a7b933fe5922180a58345e8c7
grindsa <grindelsack@gmail.com>
2020-11-17 21:54:07 +0100

    [tools] avoid that rsync overwrites permissions

--- examples/Docker/build_client/docker-entrypoint.sh
+++ examples/Docker/build_client/docker-entrypoint.sh
@@ -12,14 +12,14 @@ if [ $1 = "deploy" ]; then
     echo "deploy"
     # sync data to prod
     cd /tmp
-    rsync -avze 'ssh -i id_ed25519 -o "StrictHostKeyChecking=no"' ./rest hockeygraphs@hockeygraphs.dynamop.de:~
-    rsync -avze 'ssh -i id_ed25519 -o "StrictHostKeyChecking=no"' ./locale hockeygraphs@hockeygraphs.dynamop.de:~
-    rsync -avze 'ssh -i id_ed25519 -o "StrictHostKeyChecking=no"' ./ui/dist hockeygraphs@hockeygraphs.dynamop.de:ui
+    rsync -rvze 'ssh -i id_ed25519 -o "StrictHostKeyChecking=no"' ./rest hockeygraphs@hockeygraphs.dynamop.de:~
+    rsync -rvze 'ssh -i id_ed25519 -o "StrictHostKeyChecking=no"' ./locale hockeygraphs@hockeygraphs.dynamop.de:~
+    rsync -rvze 'ssh -i id_ed25519 -o "StrictHostKeyChecking=no"' ./ui/dist hockeygraphs@hockeygraphs.dynamop.de:ui
 
     # apply schema migrations
     ssh -i id_ed25519 hockeygraphs@hockeygraphs.dynamop.de 'python3 manage.py makemigrations && python3 manage.py migrate'
     # compile locales
-    ssh -i id_ed25519 hockeygraphs@hockeygraphs.dynamop.de 'python3 manage.py compilemessages'    
+    ssh -i id_ed25519 hockeygraphs@hockeygraphs.dynamop.de 'python3 manage.py compilemessages'
 
     # restart prod
     ssh -i id_ed25519 hockeygraphs@hockeygraphs.dynamop.de sudo '/etc/init.d/apache2 restart'
