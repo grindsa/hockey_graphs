4b322c054dd915618f3028b9bc713297b941921d
grindsa <grindelsack@gmail.com>
2020-11-20 17:04:35 +0100

    [ui] stat-selector

---
+++ ui/src/components/statselector.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class StatSelector extends React.Component {
+
+  render() {
+    return (
+      <div className = "w3-dropdown-hover w3-left w3-margin-top">
+        <div className = "pcolor">
+          <span className="w3-tag w3-round w3-border pcolor">Team benchmarking</span>
+        </div>
+      </div>
+    );
+  }
+}
