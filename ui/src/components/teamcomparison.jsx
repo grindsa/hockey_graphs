2c0fce13cf743ee281b32ea8883a449e1d93c61b
grindsa <grindelsack@gmail.com>
2020-11-24 21:12:41 +0100

    [ui] some debugging

--- ui/src/components/teamcomparison.jsx
+++ ui/src/components/teamcomparison.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 export class TeamComparison extends React.Component {
 
   render() {
+    // console.log(this.props.season, this.props.language)
     return (
         <h1> foo comp</h1>
     )
