1404ba6a095ed3f3b5d61205c52f8577c7878ef4
grindsa <grindelsack@gmail.com>
2020-12-03 18:41:53 +0100

    [rest] rebound and break in admin view

--- rest/admin.py
+++ rest/admin.py
@@ -73,8 +73,8 @@ class TeamstatAdmin(admin.ModelAdmin):
 
 class TeammatchstatAdmin(admin.ModelAdmin):
     """ admin class for teammatchstat """
-    fields = ['match', 'team', 'goals_for', 'goals_against', 'goals_pp', 'goals_sh', 'shots_for', 'shots_for_5v5', 'shots_ongoal_for', 'shots_ongoal_for_5v5', 'shots_ongoal_pctg', 'shots_against', 'shots_against_5v5', 'shots_ongoal_against', 'shots_ongoal_against_5v5', 'saves', 'saves_pctg', 'faceoffswon', 'faceoffslost', 'faceoffswon_pctg', 'penaltyminutes', 'powerplayseconds']
-    list_display = ['match', 'team', 'goals_for', 'goals_against', 'goals_pp', 'goals_sh', 'shots_for', 'shots_for_5v5', 'shots_ongoal_for', 'shots_ongoal_for_5v5', 'shots_ongoal_pctg', 'shots_against', 'shots_against_5v5', 'shots_ongoal_against', 'shots_ongoal_against_5v5', 'saves', 'saves_pctg', 'faceoffswon', 'faceoffslost', 'faceoffswon_pctg', 'penaltyminutes', 'powerplayseconds']
+    fields = ['match', 'team', 'goals_for', 'goals_against', 'goals_pp', 'goals_sh', 'shots_for', 'shots_for_5v5', 'shots_ongoal_for', 'shots_ongoal_for_5v5', 'shots_ongoal_pctg', 'shots_against', 'shots_against_5v5', 'shots_ongoal_against', 'shots_ongoal_against_5v5', 'saves', 'saves_pctg', 'faceoffswon', 'faceoffslost', 'faceoffswon_pctg', 'penaltyminutes', 'powerplayseconds', 'rebounds_for', 'rebounds_against', 'goals_rebound_for', 'goals_rebound_against', 'breaks_for', 'breaks_against', 'goals_break_for', 'goals_break_against']
+    list_display = ['match', 'team', 'goals_for', 'goals_against', 'goals_pp', 'goals_sh', 'shots_for', 'shots_for_5v5', 'shots_ongoal_for', 'shots_ongoal_for_5v5', 'shots_ongoal_pctg', 'shots_against', 'shots_against_5v5', 'shots_ongoal_against', 'shots_ongoal_against_5v5', 'saves', 'saves_pctg', 'faceoffswon', 'faceoffslost', 'faceoffswon_pctg', 'penaltyminutes', 'powerplayseconds', 'rebounds_for', 'rebounds_against', 'goals_rebound_for', 'goals_rebound_against', 'breaks_for', 'breaks_against', 'goals_break_for', 'goals_break_against']
     ordering = ['match_id', 'team_id']
 
 admin.site.register(Match, MatchAdmin)
