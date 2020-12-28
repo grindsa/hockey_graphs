54696d435f4e70f8631ca69c1618d02f0405d0b4
grindsa <grindelsack@gmail.com>
2020-12-28 12:55:35 +0100

    [tools] report generator in xg_calculate.py

--- rest/tools/xg_calculate.py
+++ rest/tools/xg_calculate.py
@@ -4,9 +4,11 @@
 import os
 import sys
 import argparse
+import json
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir)))
 
+# pylint: disable=E0401, C0413
 from rest.functions.helper import config_load, logger_setup, json_load
 from rest.functions.shot import shot_list_get
 from rest.functions.match import match_info_get, match_list_get
@@ -18,11 +20,13 @@ def arg_parse():
     parser.add_argument('-d', '--debug', help='debug mode', action="store_true", default=False)
     parser.add_argument('-m', '--model_file', help='file containing the model data', default=None)
     parser.add_argument('-q', '--quantifier_file', help='file containing weights for calculation', default=None)
+    parser.add_argument('-o', '--output_file', help='report in markdown format')
     args = parser.parse_args()
 
     debug = args.debug
     model_file = args.model_file
     quantifier_file = args.quantifier_file
+    output_file = args.output_file
 
     if not model_file:
         print('specify model file with "-m" parameter')
@@ -32,7 +36,7 @@ def arg_parse():
         print('specify quantifier file with "-q" parameter')
         sys.exit(0)
 
-    return(debug, model_file, quantifier_file)
+    return(debug, model_file, quantifier_file, output_file)
 
 def _config_load(logger, cfg_file=os.path.dirname(__file__)+'/'+'hockeygraphs.cfg'):
     """" load config from file """
@@ -50,10 +54,29 @@ def _config_load(logger, cfg_file=os.path.dirname(__file__)+'/'+'hockeygraphs.cf
 
     return (rebound_interval, break_interval)
 
+def _markdown_write(logger, output_file, output_list, quantifier_dic):
+    """ write a report """
+    logger.debug('_markdown_write()')
+
+    with open(output_file, 'w') as out_file:
+        out_file.write('# xG report\n')
+        out_file.write('## Parameter weighting\n')
+        out_file.write('```json\n')
+        json.dump(quantifier_dic, out_file, ensure_ascii=False, indent=4)
+        out_file.write('\n```\n')
+
+        out_file.write('## Match list\n')
+        out_file.write('| date | home | visitor | result  | score  | xGscore  |\n')
+        out_file.write('| :--- | :--- | :------ | :-----: | :----: | :------: |\n')
+
+        for match in output_list:
+            out_file.write('| {0} | {1} | {2} | {3} | {4} | {5} |\n'.format(match['date'], match['home'], match['visitor'], match['result'], match['score'], match['xg_score']))
+
+
 if __name__ == "__main__":
 
     # get commandline arguments
-    (DEBUG, MODEL_FILE, QUANTIFIER_FILE) = arg_parse()
+    (DEBUG, MODEL_FILE, QUANTIFIER_FILE, OUTPUT_FILE) = arg_parse()
 
     # initialize logger
     LOGGER = logger_setup(DEBUG)
@@ -69,11 +92,14 @@ if __name__ == "__main__":
     QUANTIFIER_DIC = json_load(QUANTIFIER_FILE)
 
     # get list of matches to investigate
-    MATCH_LIST = match_list_get(LOGGER, 'season_id', 1, ['match_id'])
+    MATCH_LIST = match_list_get(LOGGER, 'season_id', 3, ['match_id'])
     # MATCH_LIST = [1767]
 
     # define value list for match specific shot_dictionary
     VLIST = ['shot_id', 'match_id', 'match_shot_resutl_id', 'player_id', 'player__first_name', 'player__last_name', 'player__jersey', 'player__stick', 'team_id', 'coordinate_x', 'coordinate_y', 'match__home_team_id', 'match__visitor_team_id', 'timestamp']
+
+    # empty output listg
+    OUTPUT_LIST = []
 
     for match_id in MATCH_LIST:
 
@@ -91,15 +117,16 @@ if __name__ == "__main__":
 
         xgf_dic = xgscore_get(LOGGER, playerxgf_dic)
 
-        match_info = '{0}: {1} vs {2} ({3})'.format(match_dic['date'], match_dic['home_team__shortcut'], match_dic['visitor_team__shortcut'], match_dic['result'])
+        MATCH_INFO = '{0}: {1} vs {2} ({3})'.format(match_dic['date'], match_dic['home_team__shortcut'], match_dic['visitor_team__shortcut'], match_dic['result'])
         result = match_dic['result']
-        score = '{0}:{1}'.format(len(goal_dic['home']), len(goal_dic['visitor']))
-        xg_score = '{0}:{1}'.format(xgf_dic['home'], xgf_dic['visitor'])
+        SCORE = '{0}:{1}'.format(len(goal_dic['home']), len(goal_dic['visitor']))
+        XG_SCORE = '{0}:{1}'.format(xgf_dic['home'], xgf_dic['visitor'])
 
-        print('{0} {1}|{2}'.format(match_info, score, xg_score))
+        if OUTPUT_FILE:
+            # create list of dictionaries in case we have to write a output file
+            OUTPUT_LIST.append({'date': match_dic['date'], 'home': match_dic['home_team__shortcut'], 'visitor': match_dic['visitor_team__shortcut'], 'result': match_dic['result'], 'score': SCORE, 'xg_score': XG_SCORE})
 
+        print('{0} {1}|{2}'.format(MATCH_INFO, SCORE, XG_SCORE))
 
-
-
-        # from pprint import pprint
-        # pprint(playerxgf_dic)
+    if OUTPUT_FILE:
+        _markdown_write(LOGGER, OUTPUT_FILE, OUTPUT_LIST, QUANTIFIER_DIC)
