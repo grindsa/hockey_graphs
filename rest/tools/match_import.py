4d412cac273d5e82de852ccd4861d4df80a76ce6
grindsa <grindelsack@gmail.com>
2020-12-29 07:18:36 +0100

    [tools] match_import.py - skip past matches

--- rest/tools/match_import.py
+++ rest/tools/match_import.py
@@ -7,7 +7,7 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir)))
 import argparse
-from rest.functions.helper import logger_setup, list2dic, uts_to_date_utc
+from rest.functions.helper import logger_setup, list2dic, uts_to_date_utc, uts_now
 from rest.functions.match import match_add
 from rest.functions.season import season_latest_get, season_get
 from rest.functions.team import team_list_get
@@ -43,6 +43,8 @@ if __name__ == '__main__':
     # initialize logger
     LOGGER = logger_setup(DEBUG)
 
+    UTS_NOW = uts_now()
+
     if not SEASON_ID:
         # get season_id
         SEASON_ID = season_latest_get(LOGGER)
@@ -59,16 +61,18 @@ if __name__ == '__main__':
         game_dic = del_app_helper.games_get(TOURNAMENT_ID)
         for match in game_dic:
             if match['homeTeam'] in TEAM_DIC and match['guestTeam'] in TEAM_DIC:
-                data_dic = {
-                    'home_team_id': TEAM_DIC[match['homeTeam']]['team_id'],
-                    'visitor_team_id': TEAM_DIC[match['guestTeam']]['team_id'],
-                    'date_uts': match['dateTime'],
-                    'date': uts_to_date_utc(match['dateTime'], '%Y-%m-%d'),
-                    'season_id': SEASON_ID,
-                    'match_id': match['gameNumber']
-                }
-                (match_id, created) = match_add(LOGGER, 'match_id', match['gameNumber'], data_dic)
-                if created:
-                    LOGGER.debug('match_created({0})'.format(match_id))
-                else:
-                    LOGGER.debug('match_updated({0})'.format(match_id))
+                # skip past matches
+                if UTS_NOW <= match['dateTime']:
+                    data_dic = {
+                        'home_team_id': TEAM_DIC[match['homeTeam']]['team_id'],
+                        'visitor_team_id': TEAM_DIC[match['guestTeam']]['team_id'],
+                        'date_uts': match['dateTime'],
+                        'date': uts_to_date_utc(match['dateTime'], '%Y-%m-%d'),
+                        'season_id': SEASON_ID,
+                        'match_id': match['gameNumber']
+                    }
+                    (match_id, created) = match_add(LOGGER, 'match_id', match['gameNumber'], data_dic)
+                    if created:
+                        LOGGER.debug('match_created({0})'.format(match_id))
+                    else:
+                        LOGGER.debug('match_updated({0})'.format(match_id))
