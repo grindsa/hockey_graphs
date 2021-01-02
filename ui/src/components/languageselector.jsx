5b97cb4e03d71639e1629d856a5eeb128265411a
grindsa <grindelsack@gmail.com>
2021-01-02 20:07:16 +0100

    [ui] - languageseclector.jsx - refactor to functional component

--- ui/src/components/languageselector.jsx
+++ ui/src/components/languageselector.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 
-export class LanguageSelector extends React.Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="w3-dropdown-hover w3-right w3-margin-top w3-margin-right">
-        <div onClick={() => this.props.onClick()} className ="pcolor">
-          <span className="w3-tag w3-round w3-border pcolor">{this.props.langValue}</span>
-        </div>
+export const LanguageSelector = ({onClick, langValue}) => {
+  return (
+    <div className="w3-dropdown-hover w3-right w3-margin-top w3-margin-right">
+      <div onClick={() => onClick()} className ="pcolor">
+        <span className="w3-tag w3-round w3-border pcolor">{langValue}</span>
       </div>
-    );
-  }
+    </div>
+  );
 }
