6cf06239ffb915ce1e2eaa8fa9705059fc55d872
grindsa <grindelsack@gmail.com>
2021-01-02 20:01:20 +0100

    [ui] - canvas.jsx - refactor to functional component

--- ui/src/components/canvas.jsx
+++ ui/src/components/canvas.jsx
@@ -2,26 +2,23 @@ import React from 'react';
 import { MatchDayList } from '../components/matchday';
 import { TeamComparison } from '../components/teamcomparison';
 
-export class Canvas extends React.Component {
-
-  render() {
-    if(this.props.state.selectedStat === 0){
-      return (
-        <MatchDayList
-          matchdays={this.props.state.endpoints.matchdays}
-          matchstatistics={this.props.state.endpoints.matchstatistics}
-          language={this.props.state.language}
-          season={this.props.state.selectedSeason}
-          />
-      );
-    }else{
-      return (
-        <TeamComparison
-          teamcomparison={this.props.state.endpoints.teamcomparison}
-          language={this.props.state.language}
-          season={this.props.state.selectedSeason}
-          />
-      )
-    }
+export const Canvas = (props) => {
+  if(props.state.selectedStat === 0){
+    return (
+      <MatchDayList
+        matchdays={props.state.endpoints.matchdays}
+        matchstatistics={props.state.endpoints.matchstatistics}
+        language={props.state.language}
+        season={props.state.selectedSeason}
+        />
+    );
+  }else{
+    return (
+      <TeamComparison
+        teamcomparison={props.state.endpoints.teamcomparison}
+        language={props.state.language}
+        season={props.state.selectedSeason}
+        />
+    )
   }
 }
