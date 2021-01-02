519abf9e7f1c4395faacf7f69cfba37d34986454
grindsa <grindelsack@gmail.com>
2021-01-02 17:26:11 +0100

    [ui] - selector.jsx - refactor to functional component

--- ui/src/components/teamcomparison/selector.jsx
+++ ui/src/components/teamcomparison/selector.jsx
@@ -2,20 +2,17 @@ import React from 'react';
 import { isEmpty } from '../sharedfunctions.js';
 import { createSelectOptions }  from './teamcomparisonstateservice.js';
 
-export class Selector extends React.Component{
-  /* selector for different statistics */
-  render(){
-    if (isEmpty(this.props.stats)){
-      return (<p></p>)
-    }else{
-      const optionList = createSelectOptions(this.props.stats)
-      return (
-        <div className="w3-center my-padding-4">
-          <select className="w3-select w3-border selectbg" value={this.props.value} onChange={this.props.onChange}>
-            {optionList}
-          </select>
-        </div>
-      )
-    }
+export const Selector = ({stats, value, onChange}) => {
+  if (isEmpty(stats)){
+    return (<p></p>)
+  }else{
+    const optionList = createSelectOptions(stats)
+    return (
+      <div className="w3-center my-padding-4">
+        <select className="w3-select w3-border selectbg" value={value} onChange={onChange}>
+          {optionList}
+        </select>
+      </div>
+    )
   }
 }
