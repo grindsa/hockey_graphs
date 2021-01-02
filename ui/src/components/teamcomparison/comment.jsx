ab6bb12912670ad8ba74827800fe2d84611a5189
grindsa <grindelsack@gmail.com>
2021-01-02 17:21:03 +0100

    [ui] - comment.jsx - refactor to functional component

--- ui/src/components/teamcomparison/comment.jsx
+++ ui/src/components/teamcomparison/comment.jsx
@@ -3,17 +3,14 @@ import { isEmpty } from '../sharedfunctions.js';
 import ReactMarkdown from 'react-markdown'
 import gfm from 'remark-gfm'
 
-export class Comment extends React.Component{
-  /* selector for different statistics */
-  render(){
-    if (isEmpty(this.props.text)){
-      return (<p></p>)
-    }else{
-      return (
-        <div className="w3-container">
-          <ReactMarkdown source={this.props.text} plugins={[gfm]} />
-        </div>
-      )
-    }
+export const Comment = ({text}) => {
+  if (isEmpty(text)){
+    return (<p></p>)
+  }else{
+    return (
+      <div className="w3-container">
+          <ReactMarkdown source={text} plugins={[gfm]} />
+      </div>
+    )
   }
 }
