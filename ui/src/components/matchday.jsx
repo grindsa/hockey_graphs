86f9e32aca65b6f26e7bc02af6ac95b2e54b68fd
grindsa <grindelsack@gmail.com>
2021-01-03 16:14:25 +0100

    [ui] - matchday.jsx - refactor to functional component

--- ui/src/components/matchday.jsx
+++ ui/src/components/matchday.jsx
@@ -1,48 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { MatchStatistics } from './matchday/matchstatistics';
 import { ChangeMatchday } from './matchday/matchdaychange';
 import { changeMatchDay, checkMatchdayUpdate }  from './matchday/matchdaystateservice';
-import { asyncGET } from './sharedfunctions.js';
+import { asyncGET, isEmpty } from './sharedfunctions.js';
 
+export const MatchDayList = (props) => {
+  // class which creates and displays list of matches for cert day
+  const [matchdaydic, setMatchdaydic] = useState({})
+  const [selectedMatch, setSelectedMatch] = useState(null)
 
-export class MatchDayList extends React.Component {
-  /* class which creates and displays list of matches for cert day */
+  // initialize some variables we need
+  var mdate = ''
+  var [currentKeyName, setCurrentKeyName] = useState(null)
+  var previousKeyName = null
+  var nextKeyName = null
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      matchdaydic: {},
-      date: '',
-      currentKeyName: null,
-      previousKeyName: null,
-      nextKeyName: null,
-      selectedMatch: null,
-    };
-    this.handleMatchDayChange = this.handleMatchDayChange.bind(this);
-    this.handleMatchSelect = this.handleMatchSelect.bind(this);
-    this.resetMatchSelect = this.resetMatchSelect.bind(this);
-    this.handleDayClick = this.handleDayClick.bind(this);
-  }
-
-  async componentDidMount(){
-    if (this.props.matchdays && this.props.season) {
-      // get matchdays
-      const matchdaydic = await asyncGET(this.props.matchdays + '?season=' + this.props.season)
-      this.setState({matchdaydic: matchdaydic});
-    }
-  }
-
-  async componentDidUpdate(prevProps) {
-    /* we get the url to fectch as props and monitor it here */
-    const matchdaysupdate = checkMatchdayUpdate(this.props.matchdays, prevProps.matchdays, this.props.season, prevProps.season)
-    if (matchdaysupdate){
-        // get matchdays
-        const matchdaydic = await asyncGET(this.props.matchdays + '?season=' + this.props.season)
-        this.setState({matchdaydic: matchdaydic});
-    }
-  }
-
-  filterMatchDay(Dictionary){
+  const filterMatchDay = (Dictionary) => {
     /* filter certain matchday out of matchdaydic */
     var MatchDay = []
     for(var keyName in Dictionary){
@@ -51,84 +24,80 @@ export class MatchDayList extends React.Component {
           /* list of days will be stored in a local variable as it is not needed in states */
           MatchDay = Dictionary[keyName]['matches']
           /* we need these variables in other functions thus lets put them into state */
-          this.state.date = Dictionary[keyName]['date']
-          this.state.currentKeyName = keyName
-          this.state.previousKeyName = Dictionary[keyName]['previous']
-          this.state.nextKeyName = Dictionary[keyName]['next']
+          mdate = Dictionary[keyName]['date']
+        	currentKeyName = keyName
+          previousKeyName = Dictionary[keyName]['previous']
+          nextKeyName = Dictionary[keyName]['next']
         }
     }
-    return MatchDay
+    return  MatchDay
   }
 
-  handleMatchDayChange(newDay){
+  const handleMatchDayChange = (newDay) => {
     /* this is a handler for a matchday change we call a function and update the state */
-    this.setState(currentState => {
-      return {
-        ... currentState,
-        matchdaydic: changeMatchDay(currentState.matchdaydic, currentState.currentKeyName, newDay),
-        currentKeyName: newDay
-      }
-    })
+    setMatchdaydic(changeMatchDay(matchdaydic, currentKeyName, newDay))
+    setCurrentKeyName(newDay)
   }
 
-  handleMatchSelect(selectedMatch){
+  const handleMatchSelect = (selectedMatch) => {
     /* handler triggerd by clickcing on a single match */
-    this.setState(currentState => {
-      return {
-        ... currentState,
-        selectedMatch: selectedMatch
-      }
-    })
+    setSelectedMatch(selectedMatch)
   }
 
-  async handleDayClick(day) {
+  const handleDayClick = (day) => {
     /* handler triggered when picking a date from calendar */
     var newDate = day.toISOString().substring(0, 10)
-    await this.handleMatchDayChange(newDate)
+    handleMatchDayChange(newDate)
   }
 
-  resetMatchSelect(){
+  const resetMatchSelect = () => {
     /* handler triggered by back button in matchstatistics */
-    this.setState(currentState => {
-      return {
-        ... currentState,
-        selectedMatch: null
-      }
-    })
+    setSelectedMatch(null)
   }
 
-  render() {
-    if(!this.state.selectedMatch){
-      const MatchDay = this.filterMatchDay(this.state.matchdaydic).map((Match, index) =>{
-        return(
-          <tr key={Match.match_id} className="w3-hover-blue" onClick={() => this.handleMatchSelect(Match)}>
-            <td className="w3-right-align middle" style={{width:'35%'}}>{Match.home_team_name}</td>
-            <td className="w3-right-align middle" style={{width:'10%'}}><img src={Match.home_team_logo} alt={Match.home_team_shortcut} width="40px"/></td>
-            <td className="w3-center result middle" style={{width:'10%'}}>{Match.result} </td>
-            <td className="w3-left-align middle" style={{width:'10%'}}><img src={Match.visitor_team_logo} alt={Match.visitor_team_shortcut} width="40px"/></td>
-            <td className="w3-left-align middle" style={{width:'35%'}}>{Match.visitor_team_name}</td>
-          </tr>
-        )
-      });
-      return (
-        <React.Fragment>
-          <ChangeMatchday
-            date={this.state.date}
-            next={this.state.nextKeyName}
-            previous={this.state.previousKeyName}
-            onChangeMatchDay={this.handleMatchDayChange}
-            language={this.props.language}
-            matchdaylist = {Object.keys(this.state.matchdaydic)}
-            current={this.state.currentKeyName}
-            handleDayClick = {this.handleDayClick}
-          />
-          <table className="w3-table-all w3-hoverable"><tbody>{MatchDay}</tbody></table>
-        </React.Fragment>
-      )
-    }else{
-      return(
-        <MatchStatistics match={this.state.selectedMatch} matchstatistics={this.props.matchstatistics} reset={this.resetMatchSelect} language={this.props.language}/>
-      )
+  useEffect(() => {
+    if (props.matchdays &&  props.season) {
+      const md_get = async () => {
+        const md_dic = await asyncGET(props.matchdays + '?season=' + props.season)
+        if (!isEmpty(md_dic)){
+          setMatchdaydic(md_dic);
+        }
+      }
+      md_get()
     }
+
+  }, [props.matchdays,  props.season])
+
+  if(!selectedMatch){
+    const MatchDay = filterMatchDay(matchdaydic).map((Match, index) =>{
+      return(
+        <tr key={Match.match_id} className="w3-hover-blue" onClick={() => handleMatchSelect(Match)}>
+          <td className="w3-right-align middle" style={{width:'35%'}}>{Match.home_team_name}</td>
+          <td className="w3-right-align middle" style={{width:'10%'}}><img src={Match.home_team_logo} alt={Match.home_team_shortcut} width="40px"/></td>
+          <td className="w3-center result middle" style={{width:'10%'}}>{Match.result} </td>
+          <td className="w3-left-align middle" style={{width:'10%'}}><img src={Match.visitor_team_logo} alt={Match.visitor_team_shortcut} width="40px"/></td>
+          <td className="w3-left-align middle" style={{width:'35%'}}>{Match.visitor_team_name}</td>
+        </tr>
+      )
+    });
+    return (
+      <React.Fragment>
+        <ChangeMatchday
+          date={mdate}
+          next={nextKeyName}
+          previous={previousKeyName}
+          onChangeMatchDay={handleMatchDayChange}
+          language={props.language}
+          matchdaylist = {Object.keys(matchdaydic)}
+          current={currentKeyName}
+          handleDayClick = {handleDayClick}
+        />
+        <table className="w3-table-all w3-hoverable"><tbody>{MatchDay}</tbody></table>
+      </React.Fragment>
+    )
+  }else{
+    return(
+      <MatchStatistics match={selectedMatch} matchstatistics={props.matchstatistics} reset={resetMatchSelect} language={props.language}/>
+    )
   }
 }
