8612854417d6a9cbe410892282d4c73eac9eeced
grindsa <grindelsack@gmail.com>
2021-01-03 12:46:06 +0100

    [ui] - calendar.jsx - refactor to functional component

--- ui/src/components/matchday/calendar.jsx
+++ ui/src/components/matchday/calendar.jsx
@@ -1,50 +1,16 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
-export class Calendar extends React.Component{
+export const Calendar = (props) => {
   /* a component to show a localized calendar */
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedMatchDay: props.date,
-      lastMatchDay: null,
-      firstMatchDay: null,
-      showCalendar: false,
-      disabledDaysList: [],
-    }
-  }
+  const [firstMatchDay, setFirstMatchDay] = useState(null)
+  const [lastMatchDay, setLastMatchDay] = useState(null)
+  const [disabledDaysList, setDisabledDaysList] = useState([])
+  const [showCalendar, setShowCalendar] = useState(false)
 
-  componentDidMount(){
-    const disabledDaysList = this.createdisabledDaysList(this.props.matchdaylist.sort())
-    if (this.props.matchdaylist.length > 0){
-      this.setState({
-        firstMatchDay: this.props.matchdaylist[0],
-        lastMatchDay: this.props.matchdaylist[this.props.matchdaylist.length - 1],
-        currentMatchDay: this.props.current,
-        showCalendar: true,
-        disabledDaysList: disabledDaysList
-      });
-    }
-  }
-
-  async componentDidUpdate(prevProps){
-    if (this.props.matchdaylist !== prevProps.matchdaylist) {
-      const disabledDaysList = this.createdisabledDaysList(this.props.matchdaylist.sort())
-      if (this.props.matchdaylist.length > 0){
-        await this.setState({
-          firstMatchDay: this.props.matchdaylist[0],
-          lastMatchDay: this.props.matchdaylist[this.props.matchdaylist.length - 1],
-          currentMatchDay: this.props.current,
-          showCalendar: true,
-          disabledDaysList: disabledDaysList
-        });
-      }
-    }
-  }
-
-  createdisabledDaysList(matchdaylist){
+  const createdisabledDaysList = (matchdaylist) => {
     var disabledDaysList = []
     // first element of list with before only
     disabledDaysList.push({before: new Date(matchdaylist[0])})
@@ -59,50 +25,57 @@ export class Calendar extends React.Component{
     return disabledDaysList
   }
 
-  render() {
-    const WEEKDAYS_SHORT = { DE: ['So', 'Mon', 'Di', 'Mi', 'Do', 'Fr', 'Sa'] }
-    const MONTHS = {
-      DE: [
-        'Januar',
-        'Februar',
-        'März',
-        'Аpril',
-        'Маi',
-        'Juni',
-        'Juli',
-        'Аugust',
-        'September',
-        'Оktober',
-        'November',
-        'Dezember',
-      ]};
+  useEffect(() => {
+    if (props.matchdaylist.length > 0){
+      setFirstMatchDay(props.matchdaylist[0])
+      setLastMatchDay(props.matchdaylist[props.matchdaylist.length - 1])
+      setShowCalendar(true)
+      const disabledDaysList = createdisabledDaysList(props.matchdaylist.sort())
+      setDisabledDaysList(disabledDaysList)
+    }
+  }, [props.matchdaylist])
 
-    const WEEKDAYS_LONG = { DE: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag']};
-    const FIRST_DAY_OF_WEEK = { DE: 1};
-    const LABELS = {DE: { nextMonth: 'Nächster Monat', previousMonth: 'vorheriger Monat' }};
-    return (
-      <div className="w3-modal" style={{display: this.props.display}}>
-        <div className="w3-modal-content w3-white w3-card-4" style={{width: 350}}>
-          <div className="w3-container">
-            <span className="w3-button pcolor w3-display-topright" onClick={() => this.props.toggleCalendar()}>&times;</span>
-            <DayPicker
-              locale={this.props.language}
-              months={MONTHS[this.props.language]}
-              weekdaysLong={WEEKDAYS_LONG[this.props.language]}
-              weekdaysShort={WEEKDAYS_SHORT[this.props.language]}
-              firstDayOfWeek={1}
-              labels={LABELS[this.props.language]}
-              fromMonth={new Date(this.state.firstMatchDay)}
-              toMonth={new Date(this.state.lastMatchDay)}
-              month={new Date(this.props.current)}
-              selectedDays={[new Date(this.props.current)]}
-              disabledDays={this.state.disabledDaysList}
-              onDayClick={this.props.handleDayClick}
-            />
-          </div>
+  const WEEKDAYS_SHORT = { DE: ['So', 'Mon', 'Di', 'Mi', 'Do', 'Fr', 'Sa'] }
+  const MONTHS = {
+    DE: [
+      'Januar',
+      'Februar',
+      'März',
+      'Аpril',
+      'Маi',
+      'Juni',
+      'Juli',
+      'Аugust',
+      'September',
+      'Оktober',
+      'November',
+      'Dezember',
+    ]};
+
+  const WEEKDAYS_LONG = { DE: ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag']};
+  const FIRST_DAY_OF_WEEK = { DE: 1};
+  const LABELS = {DE: { nextMonth: 'Nächster Monat', previousMonth: 'vorheriger Monat' }};
+  return (
+    <div className="w3-modal" style={{display: props.display}}>
+      <div className="w3-modal-content w3-white w3-card-4" style={{width: 350}}>
+        <div className="w3-container">
+          <span className="w3-button pcolor w3-display-topright" onClick={() => props.toggleCalendar()}>&times;</span>
+          <DayPicker
+            locale={props.language}
+            months={MONTHS[props.language]}
+            weekdaysLong={WEEKDAYS_LONG[props.language]}
+            weekdaysShort={WEEKDAYS_SHORT[props.language]}
+            firstDayOfWeek={1}
+            labels={LABELS[props.language]}
+            fromMonth={new Date(firstMatchDay)}
+            toMonth={new Date(lastMatchDay)}
+            month={new Date(props.current)}
+            selectedDays={[new Date(props.current)]}
+            disabledDays={disabledDaysList}
+            onDayClick={props.handleDayClick}
+          />
         </div>
       </div>
-    )
-
-  }
+    </div>
+  )
 }
