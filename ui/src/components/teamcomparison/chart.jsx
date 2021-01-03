87b08e78aeec33156bb0264b049bbc3c0595f345
grindsa <grindelsack@gmail.com>
2021-01-03 09:23:15 +0100

    [ui] - chart.jsx - refactor to functional component

--- ui/src/components/teamcomparison/chart.jsx
+++ ui/src/components/teamcomparison/chart.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import { isMobile } from 'react-device-detect';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
@@ -20,79 +20,64 @@ HighchartsMore(Highcharts);
 Heatmap(Highcharts);
 AnnotationsModule(Highcharts);
 
-export class Chart extends React.Component{
+export const Chart = (props) => {
   /* block to render chart mobile differenciation is done via chartoptions */
-  constructor(props) {
-    super(props);
-    this.state = props.options
 
-    this.updateChart = this.updateChart.bind(this);
-    this.handleSliderChange = this.handleSliderChange.bind(this);
-  }
+  const [slidervalue, setSlidervalue] = useState(Object.keys(props.options.updates).length);
+  const [chart, setChart] = useState(props.options.chart);
+  const [updates, setUpdates] = useState(props.options.updates)
 
-  componentDidMount(){
-    // set initial slidervalue
-    this.setState({slidervalue: Object.keys(this.props.options.updates).length});
-  }
-
-  componentDidUpdate(prevProps){
-    if (prevProps.options !== this.props.options){
-      this.setState(this.props.options);
-      // set initial slidervalue
-      this.setState({slidervalue: Object.keys(this.props.options.updates).length});
-    }
-  }
-
-  updateChart(newData){
-    // update chart in state
-    const newChart = Object.assign({}, this.state.chart, newData)
-    this.setState({chart: newChart });
-  }
-
-  async handleSliderChange(newvalue){
+  const handleSliderChange = async (newvalue) => {
     // update slidervalue after usage
-    await this.setState(currentState => {
-      return {
-      ... currentState,
-      slidervalue: newvalue,
-      }
-    });
-    this.updateChart(this.state.updates[this.state.slidervalue].chartoptions)
+    await setSlidervalue(newvalue)
+    // update chart after sliderchange
+    updateChart(updates[newvalue].chartoptions)
   }
 
-  render() {
-    if (this.state.chart){
-      // get the number of dataupdates as this will be the max-val for the slider
-      const slidermaxval = Object.keys(this.props.options.updates).length
-      const slidertext = createTcSliderText(this.props.language, this.state.slidervalue, slidermaxval)
-      var className = ""
+  const updateChart = (newData) => {
+    // update chart
+    const newChart = Object.assign({}, chart, newData);
+    setChart(newChart);
+  }
 
-      if (isMobile) {
-        var classNames = "w3-center w3-margin-left w3-margin-right"
-      }
-      return (
-        <React.Fragment>
-          <Slider
-            min={1}
-            max={slidermaxval}
-            value = {this.state.slidervalue}
-            step={1}
-            orientation='horizontal'
-            onChange={this.handleSliderChange}
-            className={classNames}
-          />
-          <div className="w3-center w3-margin-bottom">{slidertext}</div>
-          <div className="w3-border">
-            <HighchartsReact highcharts={Highcharts} options={this.state.chart} immutable={true}/>
-          </div>
-          <Comment text={this.props.options.comment} />
-        </React.Fragment>
-      )
-    }else{
-      const nochartdata = createnoChartMessage(this.props.language)
-      return (
-        <div className="w3-padding-16 nodata w3-center">{nochartdata}</div>
-      )
+  useEffect(() => {
+    // this is a chart-change - we also need to reset slidervalue and updates "updates"
+    setChart(props.options.chart)
+    setSlidervalue(Object.keys(props.options.updates).length)
+    setUpdates(props.options.updates)
+  }, [props.options.chart])
+
+  if (chart){
+    // get the number of dataupdates as this will be the max-val for the slider
+    const slidermaxval = Object.keys(props.options.updates).length
+    const slidertext = createTcSliderText(props.language, slidervalue, slidermaxval)
+    var className = ""
+
+    if (isMobile) {
+      var classNames = "w3-center w3-margin-left w3-margin-right"
     }
+    return (
+      <React.Fragment>
+        <Slider
+          min={1}
+          max={slidermaxval}
+          value = {slidervalue}
+          step={1}
+          orientation='horizontal'
+          onChange={handleSliderChange}
+          className={classNames}
+        />
+        <div className="w3-center w3-margin-bottom">{slidertext}</div>
+        <div className="w3-border">
+          <HighchartsReact highcharts={Highcharts} options={chart} immutable={true}/>
+        </div>
+        <Comment text={props.options.comment} />
+      </React.Fragment>
+    )
+  }else{
+    const nochartdata = createnoChartMessage(props.language)
+    return (
+      <div className="w3-padding-16 nodata w3-center">{nochartdata}</div>
+    )
   }
 }
