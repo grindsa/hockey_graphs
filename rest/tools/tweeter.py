4c362f1605b0dd0f8415b701f611c5a995bba4ab
grindsa <grindelsack@gmail.com>
2021-10-23 20:40:46 +0200

    [tools] tweeter.py - modified text for TOI message

--- rest/tools/tweeter.py
+++ rest/tools/tweeter.py
@@ -254,7 +254,7 @@ def twitter_it(logger, matchinfo_dic_, img_list_, season_id, match_id_):
     chart_list = ['Charts', 'bunte Bildchen', 'Grafiken', 'Chartz']
     match_date = uts_to_date_utc(matchinfo_dic_['date_uts'], '%d.%m.%Y')
     text_initial = 'Hier ein paar {2} zum Spiel {0} gg. {1}. vom {5} (Endstand: {6}).\nMehr unter https://hockeygraphs.dynamop.de/matchstatistics/{3}/{4} ...'.format(matchinfo_dic_['home_team__shortcut'].upper(), matchinfo_dic['visitor_team__shortcut'].upper(), random.choice(chart_list), season_id, match_id_, match_date, matchinfo_dic_['result_full'])
-    text_reply = 'Und noch die Eiszeiten pro Spieler pro Drittel und in Über- und Unterzahl... {0}'.format(tags)
+    text_reply = 'Und noch die Eiszeiten pro Spieler sowie die Eiszeiten in Über- und Unterzahl... {0}'.format(tags)
 
     # LogIn
     twitter_uploader = twitter_login(logger, consumer_key, consumer_secret, oauth_token, oauth_token_secret, 'upload.twitter.com')
