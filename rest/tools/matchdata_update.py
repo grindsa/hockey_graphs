e55118b845ff220d9cc224a3c3763967ae750f42
grindsa <grindelsack@gmail.com>
2022-09-16 07:34:14 +0200

    [tools] save_dir

--- rest/tools/matchdata_update.py
+++ rest/tools/matchdata_update.py
@@ -350,12 +350,12 @@ if __name__ == '__main__':
                 json_store('{0}/{1}'.format(MATCH_DIR, 'shots.json'), shots_dic)
                 if ADDSHIFTS:
                     json_store('{0}/{1}'.format(MATCH_DIR, 'shifts.json'), shift_dic)
-                    if HGS_DATA and not os.path.exists('{0}/shifts/2021/1/{1}.json'.format(HGS_DATA, match_id)):
+                    if HGS_DATA and not os.path.exists('{0}/shifts/2022/1/{1}.json'.format(HGS_DATA, match_id)):
                         LOGGER.debug('fetch shifts')
-                        json_store('{0}/shifts/2021/1/{1}.json'.format(HGS_DATA, match_id), shift_dic)
-                    if HGS_DATA and not os.path.exists('{0}/shots/2021/1/{1}.json'.format(HGS_DATA, match_id)):
+                        json_store('{0}/shifts/2022/1/{1}.json'.format(HGS_DATA, match_id), shift_dic)
+                    if HGS_DATA and not os.path.exists('{0}/shots/2022/1/{1}.json'.format(HGS_DATA, match_id)):
                         LOGGER.debug('fetch shots')
-                        json_store('{0}/shots/2021/1/{1}.json'.format(HGS_DATA, match_id), shots_dic)
+                        json_store('{0}/shots/2022/1/{1}.json'.format(HGS_DATA, match_id), shots_dic)
 
     if GITREPO and SAVE:
         # check changes into repo
